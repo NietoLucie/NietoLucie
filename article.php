6aa5b1f5fa6ee47cf87fc9edb14b2cc8ef8563f0
McAsulys <benedict.dourlens@gmail.com>
2017-09-13 16:00:34 +0200

    Ajout de la fonction article à modifier. Ajout du css pour les articles

---
+++ article.php
@@ -0,0 +1,26 @@
+<?php
+function article($contenue){
+  /*
+  le contenue est un tableau contenant le texte, les image et
+  autre élément html/css du site
+  l'index doit suivre la regle suivante :
+    title => titre de l'article
+    p => paragraphe
+    img => image simple !! fournir l'url
+    hat => chapeau de l'article
+    o => other, pour les autre !! fournir code html/css complet
+  */
+
+  $article = "<div class='article'>";
+  foreach ($contenue as $key => $value) {
+    if($key=="p") $article .= "<p>$value</p>";
+    elseif ($key=="title") $article .= "<h2>$value</h2>";
+    elseif ($key=="hat") $article .= "<span class='chapeau'>$value</span>";
+    elseif ($key=="img") $article .= "<img src='$value'>";
+    elseif ($key=="o") $article .= "$value";
+    else $article .= "<p class='error'>ERROR index non reconue : $key</p>";
+  }
+  $article .= "</div>";
+  echo $article;
+}
+ ?>
--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <DOCTYPE! html>
 <html>
 
-<?php
-  include "header.php";
-  include "head.php";
-?>
+<?php include "header.php"; ?>
 
+<body>
+  <?php  include "head.php"; ?>
+
+
+  <div id="articles">
+    <?php
+    include "article.php";
+
+      $article1 = [
+        "title" => "article premier",
+        "hat" => "ceci est le premier article de ce site",
+        "img" => "http://www.vulgaris-medical.com/sites/default/files/styles/big-lightbox/public/field/image/actualites/2016/02/12/le-chat-source-de-bienfaits-pour-votre-sante.jpg",
+        "p" => "Bonjour à tous, cecis est une photo de chat. C'est beau mais voilà, on s'en fout.
+        le but est de produir un artcticle fake. Pour fournir tout ça.",
+        "img" => "https://www.sciencesetavenir.fr/assets/img/2016/09/02/cover-r4x3w1000-57e16b966fe58-chat-qui-mange.jpg",
+        "p" => "voilà un nouveaux chat qui va pour manger des croquette, bon bref, encore une fois
+        on s'en fout, c'est juste pour le teste."
+      ];
+
+      article($article1);
+
+     ?>
+  </div>
+</body>
 
 </html>
