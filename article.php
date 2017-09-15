191ad6e4f9355c6a8a84007541ca36ff011cc619
McAsulys <benedict.dourlens@gmail.com>
2017-09-15 17:34:57 +0200

    Retrait de markdown, retour au HTMl classique Affinage du CSS en cour

--- article.php
+++ article.php
@@ -1,20 +1,27 @@
 <?php
-function article($name){
-  require_once "Parsedown.php";
-  require_once "ParsedownExtra.php";
-  require_once "ParsedownExtraPlugin.php";
-  //require_once "markdown.php";
-
-  $parsedown = new ParsedownExtraPlugin();
-  $parsedown->code_class = 'lang-%s';
-  $text = fopen("article/".$name.".txt", "r");
-  echo $parsedown->text($text);
-
-
-
-
+function AllArticle(){
+  $tab = scanDirectory("article");
+  foreach ($tab as $key => $value) {
+    article($value);
+  }
 }
 
+function article($name){
+  echo file_get_contents("article/"."$name");
+}
 
-
+function scanDirectory($dir){
+$tab = array();
+if (is_dir($dir)) {
+   if ($dh = opendir($dir)) {
+       while (($file = readdir($dh)) !== false) {
+           if( $file != '.' && $file != '..') {
+           $tab[] = "$file";
+           }
+       }
+       closedir($dh);
+       return $tab;
+   }
+}
+}
  ?>
--- index.php
+++ index.php
@@ -11,31 +11,7 @@
     <?php
     include "article.php";
 
-
-/*    $fichierArticle = simplexml_load_file("article/chat.xml");
-
-
-  $json = json_encode($fichierArticle);
-  $array = json_decode($json,TRUE);
-
-echo "<pre>".print_r($array)."</pre>"; //die(1);
-$article = "<div class='article'>";
-    foreach ($array as $key=>$value) {
-      if($key=="p") $article .= "<p>$value</p>";
-      elseif ($key=="titre") $article .= "<h2>$value</h2>";
-      elseif ($key=="chapeau") $article .= "<span class='chapeau'>$value</span>";
-      elseif ($key=="img") {
-        $article .="<img";
-        foreach ($value as $key => $value) {
-          if ($key=="name") $article .="alt='$value'";
-          elseif ($key=="url") $article .="src='$value'";
-        }
-      }
-      elseif ($key=="o") $article .= "$value";
-      else $article .= "<p class='error'>ERROR index non reconue : $key</p>";
-    }*/
-
-article("chat");
+    AllArticle();
 
      ?>
   </div>
