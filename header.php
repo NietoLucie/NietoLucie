c68acdea576a8ad9d9957258f1b47d2de89a0505
McAsulys <benedict.dourlens@gmail.com>
2017-09-19 11:37:23 +0200

    Ajout des media en css pour la responsivité.

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <header>
   <title>Les création du Poney</title>
   <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link type="text/css" rel="stylesheet" href="index.css">
   <?php
-    
+
 
    ?>
 </header>
