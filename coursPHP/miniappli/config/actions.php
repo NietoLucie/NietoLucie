bb5bc22617579549f4773f79aabc292b10053227
McAsulys <benedict.dourlens@gmail.com>
2017-10-09 08:52:16 +0200

    ajout du mini projet

---
+++ coursPHP/miniappli/config/actions.php
@@ -0,0 +1,7 @@
+<?php
+// Voici la liste des actions possibles avec la page à charger associée
+
+$listeDesActions = array(
+    "accueil" => "vues/accueil.php",
+    "page2" => "vues/page2.php",
+);
--- coursPHP/tp4.php
+++ coursPHP/tp4.php
@@ -5,3 +5,5 @@ setcookie("test",10,time()+60);
  ?>
 
 <a href="tp4a.php">tp4a</a>
+
+<!-- exo 3 -->
