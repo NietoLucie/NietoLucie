f398cc4627788bddee310063cb78e81034093414
NietoLucie <lucie.nieto@102f03.iut-lens.univ-artois.fr>
2017-10-16 10:20:39 +0200

    completation de index.php

---
+++ myfb/index.php
@@ -0,0 +1,44 @@
+<?php
+
+	include("config/config.php");
+	include("config/bd.php");
+	session_start();
+	
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			
+			
+    <title>Mon Facebook</title>
+		<!-- Bootstrap core CSS -->
+		<link href="./css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<link href="./css/ie10.css" rel="stylesheet">
+
+
+		<!-- Ma feuille de style à moi -->
+		<link href="./css/style.css" rel="stylesheet">
+
+		<script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+		<header>
+			<h3>Mon Facebook à moi !</h3>
+		</header>
+		
+		<div class="amis">
+			Vos amis
+		</div>
+	
+	
+		
+	</body>
+</html>
